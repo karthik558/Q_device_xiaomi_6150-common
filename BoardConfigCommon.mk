009e98c12fca1f799c5e11fd5d09856b253018fd
Karthik <karthik.lal558@gmail.com>
2020-09-25 15:49:06 +0000

    Set SELINUX_IGNORE_NEVERALLOWS=false

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -205,6 +205,7 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS=false
 
 # WiFi
 BOARD_HOSTAPD_DRIVER := NL80211
